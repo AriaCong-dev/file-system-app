253a2edb7c94dba383b20cadd50db62e806309ed
Aria Cong <rongbehappy@gmail.com>
2025-04-13 19:59:02 +1000

    json viewer and its style is added

--- src/components/JsonViewer.tsx
+++ src/components/JsonViewer.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import styles from './JsonViewer.module.scss'
-import useTreeNodeService from '../hooks/useTreeNodeService'
+import { useTreeContext } from '../contexts/TreeContext'
 
 const JsonViewer: React.FC = () => {
-  return <div></div>
+  return (
+    <div className={styles.jsonViewerContainer}>
+      <h2 className={styles.title}>JSON Viewer</h2>
+      <textarea
+        readOnly
+        className={styles.jsonViewer}
+        value={JSON.stringify(useTreeContext().treeArray, null, 2)}
+      />
+    </div>
+  )
 }
 
 export default JsonViewer
